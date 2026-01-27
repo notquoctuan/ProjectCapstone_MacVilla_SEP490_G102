171bb6ead9118612de5fff3ab82dc1955ac93bf2
Ducnahe170599 <ducnahe170599@fpt.edu.vn>
2026-01-28 00:30:47 +0700

    Add Swagger
    Add Swagger

--- MacVilla/Presentation/Controllers/WeatherForecastController.cs
+++ MacVilla/Presentation/Controllers/WeatherForecastController.cs
@@ -1,22 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Domain.Entities; 
 
 namespace Presentation.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : ControllerBase 
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        private static readonly string[] Summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
