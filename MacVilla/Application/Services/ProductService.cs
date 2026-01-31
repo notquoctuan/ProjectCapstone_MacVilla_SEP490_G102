f965aed493ebc6752801b6f1ca49e10eb36da401
Ducnahe170599 <ducnahe170599@fpt.edu.vn>
2026-02-01 01:09:24 +0700

    up view lisst product

---
+++ MacVilla/Application/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Application.DTOs;
+
+namespace Application.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _productRepo;
+        public ProductService(IProductRepository productRepo) => _productRepo = productRepo;
+
+        public async Task<IEnumerable<ProductAdminResponse>> SearchProductsForAdmin(string? name, decimal? min, decimal? max, int? catId)
+        {
+            var products = await _productRepo.GetProductsForAdminAsync(name, min, max, catId);
+
+            return products.Select(p => new ProductAdminResponse
+            {
+                ProductId = p.ProductId,
+                Name = p.Name,
+                CategoryName = p.Category?.CategoryName,
+                Price = p.Price ?? 0,
+                Status = p.Status,  
+                CreatedAt = p.CreatedAt
+            });
+        }
---
+++ MacVilla/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsForAdminAsync(string? name, decimal? minPrice, decimal? maxPrice, int? categoryId);
