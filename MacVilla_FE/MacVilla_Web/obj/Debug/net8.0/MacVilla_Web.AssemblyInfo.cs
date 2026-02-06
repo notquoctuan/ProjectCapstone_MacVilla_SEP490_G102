28bcb9f65d820c7e326110e3388e8e2166f21c23
Ducnahe170599 <ducnahe170599@fpt.edu.vn>
2026-02-07 01:33:41 +0700

    UpdateIndex
    a

--- MacVilla_FE/MacVilla_Web/obj/Debug/net8.0/MacVilla_Web.AssemblyInfo.cs
+++ MacVilla_FE/MacVilla_Web/obj/Debug/net8.0/MacVilla_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MacVilla_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6291037a013efb6a9f826cfd2d5d1b573637c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("MacVilla_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MacVilla_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
