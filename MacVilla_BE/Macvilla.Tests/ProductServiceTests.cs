c40bc8f996fe4d0cb620de48bf2294924f6335e3
thienanh411 <ldthienanh@gmail.com>
2026-03-19 15:53:53 +0700

    add test for product service

--- MacVilla_BE/Macvilla.Tests/ProductServiceTests.cs
+++ MacVilla_BE/Macvilla.Tests/ProductServiceTests.cs
@@ -1,75 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
-using System.Threading.Tasks;
 using Application.DTOs;
 using Application.Services;
 using Domain.Entities;
 using Domain.Interfaces;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
-using Moq;
-using Xunit;
 using Microsoft.AspNetCore.Http;
+using MockQueryable;
+using MockQueryable.Moq;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace MacVilla.Tests
 {
     public class ProductServiceTests
     {
-        #region EF Core Async Helpers
-        internal class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
-        {
-            private readonly IQueryProvider _inner;
-            public TestAsyncQueryProvider(IQueryProvider inner) => _inner = inner;
-            public IQueryable CreateQuery(Expression expression) => new TestAsyncEnumerable<TEntity>(expression);
-            public IQueryable<TElement> CreateQuery<TElement>(Expression expression) => new TestAsyncEnumerable<TElement>(expression);
-            public object? Execute(Expression expression) => _inner.Execute(expression);
-            public TResult Execute<TResult>(Expression expression) => _inner.Execute<TResult>(expression);
-            public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
-            {
-                var expectedResultType = typeof(TResult).GetGenericArguments()[0];
-
-                var executionResult = typeof(IQueryProvider)
-                    .GetMethod(
-                        name: nameof(IQueryProvider.Execute),
-                        genericParameterCount: 1,
-                        types: new[] { typeof(Expression) })!
-                    .MakeGenericMethod(expectedResultType)
-                    .Invoke(_inner, new object[] { expression });
-
-                return (TResult)typeof(Task)
-                    .GetMethod(nameof(Task.FromResult))!
-                    .MakeGenericMethod(expectedResultType)
-                    .Invoke(null, new[] { executionResult })!;
-            }
-        }
-            
-        internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
-        {
-            public TestAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
-            public TestAsyncEnumerable(Expression expression) : base(expression) { }
-            public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default) => new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
-            IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
-        }
-
-        internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
-        {
-            private readonly IEnumerator<T> _inner;
-            public TestAsyncEnumerator(IEnumerator<T> inner) => _inner = inner;
-            public ValueTask DisposeAsync() { _inner.Dispose(); return default; }
-            public ValueTask<bool> MoveNextAsync() => new ValueTask<bool>(_inner.MoveNext());
-            public T Current => _inner.Current;
-        }
-        #endregion
-
         private readonly Mock<IProductRepository> _productRepoMock;
         private readonly Mock<ICategoryRepository> _categoryRepoMock;
         private readonly ProductService _service;
+        private const string WebRootPath = "wwwroot";
 
         public ProductServiceTests()
         {
@@ -78,18 +29,16 @@ namespace MacVilla.Tests
             _service = new ProductService(_productRepoMock.Object, _categoryRepoMock.Object);
         }
 
-        // --- CREATE TESTS ---
-
+        #region TC5 & TC6: CreateProductAsync
         [Fact]
-        public async Task CreateProductAsync_ShouldThrow_WhenCategoryNotExist()
+        public async Task CreateProductAsync_ShouldThrowException_WhenCategoryDoesNotExist()
         {
-            // Arrange
-            _categoryRepoMock.Setup(r => r.GetByIdAsync(It.IsAny<long>())).ReturnsAsync((Category?)null);
-
-            var request = new ProductCreateRequest { CategoryId = 999, Name = "Test", Price = 100 };
+            // Arrange (TC5)
+            _categoryRepoMock.Setup(r => r.GetByIdAsync(It.IsAny<long>())).ReturnsAsync((Category)null);
+            var request = new ProductCreateRequest { CategoryId = 99, Name = "Test" };
 
             // Act
-            Func<Task> act = async () => await _service.CreateProductAsync(request, "dummyPath");
+            Func<Task> act = async () => await _service.CreateProductAsync(request, WebRootPath);
 
             // Assert
             await act.Should().ThrowAsync<KeyNotFoundException>().WithMessage("*không tồn tại*");
@@ -98,116 +47,108 @@ namespace MacVilla.Tests
         [Fact]
         public async Task CreateProductAsync_ShouldReturnResponse_WhenValid()
         {
-            // Arrange
-            var category = new Category { CategoryId = 1, CategoryName = "Sanitary" };
+            // Arrange (TC6)
+            var category = new Category { CategoryId = 1, CategoryName = "Sơn" };
+            var request = new ProductCreateRequest { CategoryId = 1, Name = "Sơn Dulux", Price = 100000 };
+
             _categoryRepoMock.Setup(r => r.GetByIdAsync(1)).ReturnsAsync(category);
             _productRepoMock.Setup(r => r.ExistsByNameAsync(It.IsAny<string>(), null)).ReturnsAsync(false);
-
-            _productRepoMock.Setup(r => r.AddAsync(It.IsAny<Product>()))
-                .ReturnsAsync((Product p) => { p.ProductId = 500; return p; });
-
-            var request = new ProductCreateRequest
-            {
-                Name = "Lavabo Luxury",
-                CategoryId = 1,
-                Price = 1500,
-                Status = "Active"
-            };
+            _productRepoMock.Setup(r => r.AddAsync(It.IsAny<Product>())).ReturnsAsync((Product p) => {
+                p.ProductId = 10; // Giả lập DB sinh ID
+                return p;
+            });
 
             // Act
-            var result = await _service.CreateProductAsync(request, "tempPath");
+            var result = await _service.CreateProductAsync(request, WebRootPath);
 
             // Assert
-            result.Should().NotBeNull();
-            result.ProductId.Should().Be(500);
-            result.Name.Should().Be("Lavabo Luxury");
-            result.CategoryName.Should().Be("Sanitary");
+            result.ProductId.Should().Be(10);
+            result.Name.Should().Be("Sơn Dulux");
+            result.CategoryName.Should().Be("Sơn");
         }
+        #endregion
 
-        // --- GET PAGED TESTS ---
-
+        #region TC9, TC10: UpdateProductAsync
         [Fact]
-        public async Task GetPagedProductsAsync_ShouldReturnPagedResult()
+        public async Task UpdateProductAsync_ShouldThrowException_WhenProductNotFound()
         {
-            // Arrange
-            var products = new List<Product>
-            {
-                new Product { ProductId = 1, Name = "P1", Price = 10, CreatedAt = DateTime.UtcNow, Category = new Category{ CategoryName="C1"} },
-                new Product { ProductId = 2, Name = "P2", Price = 20, CreatedAt = DateTime.UtcNow, Category = new Category{ CategoryName="C1"} }
-            }.AsQueryable();
-
-            var asyncQuery = new TestAsyncEnumerable<Product>(products);
-            _productRepoMock.Setup(r => r.GetQueryable()).Returns(asyncQuery);
-
-            var request = new ProductSearchRequest { PageNumber = 1, PageSize = 10 };
-
+            // Arrange (TC9)
+            _productRepoMock.Setup(r => r.GetByIdAsync(It.IsAny<long>())).ReturnsAsync((Product)null);
+            var request = new ProductUpdateRequest { CategoryId = 1, Name = "Update" };
             // Act
-            var result = await _service.GetPagedProductsAsync(request);
-
+            Func<Task> act = async () => await _service.UpdateProductAsync(1, request, WebRootPath);
             // Assert
-            result.Data.Should().HaveCount(2);
-            result.TotalCount.Should().Be(2);
+            await act.Should().ThrowAsync<KeyNotFoundException>();
         }
+        #endregion
 
-        // --- UPDATE TESTS ---
-
+        #region TC11, TC12: ChangeProductStatus
         [Fact]
-        public async Task UpdateProductAsync_ShouldThrow_WhenProductNotFound()
+        public async Task ChangeStatusAsync_ShouldThrowException_WhenStatusIsInvalid()
         {
-            // Arrange
-            _productRepoMock.Setup(r => r.GetByIdAsync(It.IsAny<long>())).ReturnsAsync((Product?)null);
+            // Arrange (TC11)
+            var invalidStatus = "WrongStatus";
 
             // Act
-            Func<Task> act = async () => await _service.UpdateProductAsync(1, new ProductUpdateRequest(), "path");
+            Func<Task> act = async () => await _service.ChangeStatusAsync(1, invalidStatus);
 
             // Assert
-            await act.Should().ThrowAsync<KeyNotFoundException>().WithMessage("*Không tìm thấy sản phẩm*");
+            await act.Should().ThrowAsync<ArgumentException>().WithMessage("*Trạng thái không hợp lệ*");
         }
 
         [Fact]
-        public async Task UpdateProductAsync_ShouldCallUpdate_WhenValid()
+        public async Task ChangeStatusAsync_ShouldReturnTrue_WhenValidStatus()
         {
-            // Arrange
-            var existing = new Product { ProductId = 1, Name = "Old", ProductImages = new List<ProductImage>() };
-            _productRepoMock.Setup(r => r.GetByIdAsync(1)).ReturnsAsync(existing);
-            _categoryRepoMock.Setup(r => r.GetByIdAsync(It.IsAny<long>())).ReturnsAsync(new Category { CategoryName = "Cat" });
-
-            var request = new ProductUpdateRequest { Name = "New Name", CategoryId = 2, Price = 200 };
+            // Arrange (TC12)
+            _productRepoMock.Setup(r => r.UpdateStatusAsync(1, "Enable")).ReturnsAsync(true);
 
             // Act
-            await _service.UpdateProductAsync(1, request, "path");
+            await _service.ChangeStatusAsync(1, "Enable");
 
             // Assert
-            _productRepoMock.Verify(r => r.ExistsByNameAsync("New Name", 1), Times.Once);
+            _productRepoMock.Verify(r => r.UpdateStatusAsync(1, "Enable"), Times.Once);
+        }
+        #endregion
+
+        #region TC13, TC14: GetProductDetail
+        [Fact]
+        public async Task GetProductDetailAsync_ShouldReturnNull_WhenProductDoesNotExist()
+        {
+            // Arrange (TC13)
+            _productRepoMock.Setup(r => r.GetByIdDetailAsync(99)).ReturnsAsync((Product)null);
+
+            // Act
+            var result = await _service.GetProductDetailAsync(99);
+
+            // Assert
+            result.Should().BeNull();
         }
 
-        // --- DETAIL TESTS ---
-
         [Fact]
-        public async Task GetProductDetailAsync_ShouldReturnMappedDto_WhenFound()
+        public async Task GetProductDetailAsync_ShouldReturnCorrectData_IncludingImages()
         {
-            // Arrange
+            // Arrange (TC14)
             var product = new Product
             {
-                ProductId = 10,
-                Name = "Bồn cầu",
-                Price = 5000,
-                Category = new Category { CategoryName = "Thiết bị vệ sinh" },
+                ProductId = 1,
+                Name = "Sản phẩm 1",
                 ProductImages = new List<ProductImage>
                 {
-                    new ProductImage { ImageId = 1, ImageUrl = "img.jpg", IsMain = true }
+                    new ProductImage { ImageId = 101, ImageUrl = "img1.jpg", IsMain = true },
+                    new ProductImage { ImageId = 102, ImageUrl = "img2.jpg", IsMain = false }
                 }
             };
-            _productRepoMock.Setup(r => r.GetByIdDetailAsync(10)).ReturnsAsync(product);
+            _productRepoMock.Setup(r => r.GetByIdDetailAsync(1)).ReturnsAsync(product);
 
             // Act
-            var result = await _service.GetProductDetailAsync(10);
+            var result = await _service.GetProductDetailAsync(1);
 
             // Assert
             result.Should().NotBeNull();
-            result!.Name.Should().Be("Bồn cầu");
-            result.CategoryName.Should().Be("Thiết bị vệ sinh");
-            result.Images.Should().ContainSingle(i => i.IsMain);
+            result.Name.Should().Be("Sản phẩm 1");
+            result.Images.Should().HaveCount(2);
+            result.Images.First(i => i.IsMain).ImageUrl.Should().Be("img1.jpg");
         }
+        #endregion
     }
 }
